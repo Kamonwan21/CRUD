4963d498b59b1a7267287409893ca8b49153dcf1
Kamonwan21 <mindinmyheat@gmail.com>
2023-08-31 15:39:45 +0700

    Yeah this is first commit

---
+++ flutter1/lib/models/config.dart
@@ -0,0 +1,7 @@
+import 'package:screen/models/users.dart';
+
+class Configure {
+  static const server = "10.116.0.231:3000";
+  static Users login = Users();
+  static List<String> gender = ["None", "Male", "Female"];
+}
---
+++ flutter1/lib/screen/sidemenu.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:screen/screen/home.dart';
+import 'package:screen/screen/login.dart';
+
+import '../models/config.dart';
+import '../models/users.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String accountName = "N/A";
+    String accountEmail = "N/A";
+    String accountUrl =
+        "https://i.pinimg.com/736x/28/9b/4c/289b4c830d664657a83ccfde6ae5b268.jpg";
+
+    Users user = Configure.login;
+    //print(user.toJson().toString());
+    if (user.id != null) {
+      accountName = user.fullname!;
+      accountEmail = user.email!;
+    }
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(accountName),
+            accountEmail: Text(accountEmail),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(accountUrl),
+              backgroundColor: Colors.white,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pushNamed(context, Home.routeName);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text('Login'),
+            onTap: () {
+              Navigator.pushNamed(context, Login.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
